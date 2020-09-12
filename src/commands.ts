1d368cb2a413d2b62904777dfc71f67d636391ea
Vildan Softic <zewa666@gmail.com>
2020-09-12 12:05:10 +0200

    refactor: extract commands and proper indent

---
+++ src/commands.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { kebabCase, pascalCase } from "case-anything";
+
+export function createCustomElementCommand(context: vscode.ExtensionContext) {
+  return async (uri: vscode.Uri) => {
+    const ceName = await vscode.window.showInputBox({
+      prompt: "Enter the new custom elements name"
+    });
+    const storedLanguage = context.workspaceState.get<string>("aurelia-utils-ce-language") || "ts";
+    const language = await vscode.window.showQuickPick([storedLanguage, storedLanguage === "ts" ? "js" : "ts"], { ignoreFocusOut: true });
+    context.workspaceState.update("aurelia-utils-ce-language", language);
+
+    if (!ceName) {
+      return;
+    }
+
+    const vmFile = vscode.Uri.parse('untitled:' + path.join(uri.fsPath, `${kebabCase(ceName)}.${language}`));
+    vscode.workspace.openTextDocument(vmFile).then(document => {
+      const edit = new vscode.WorkspaceEdit();
+      edit.insert(vmFile, new vscode.Position(0, 0), `import { autoinject } from "aurelia-framework";
+
+@autoinject()
+export class ${pascalCase(ceName)} {
+  constructor() {}
+}`);
+      return vscode.workspace.applyEdit(edit).then(success => {
+        if (success) {
+          vscode.window.showTextDocument(document);
+        } else {
+          vscode.window.showInformationMessage('Error creating VM for custom element!');
+        }
+      });
+    });
+
+    const viewFile = vscode.Uri.parse('untitled:' + path.join(uri.fsPath, `${kebabCase(ceName)}.html`));
+    vscode.workspace.openTextDocument(viewFile).then(document => {
+      const edit = new vscode.WorkspaceEdit();
+      edit.insert(viewFile, new vscode.Position(0, 0), `<template>
+
+</template>`);
+      return vscode.workspace.applyEdit(edit).then(success => {
+        if (success) {
+          vscode.window.showTextDocument(document);
+        } else {
+          vscode.window.showInformationMessage('Error creating View for custom element!');
+        }
+      });
+    });
+  };
+}
